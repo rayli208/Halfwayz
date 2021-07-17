72fbbb92b7fb9d8fa14d7beebc148393fc4e224a
Rayli Raykov <rayli.raykov@gmail.com>
2021-07-17 18:50:09 -0400

    temp save

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //ON LOAD FIND LOCATION
 google.maps.event.addDomListener(window, "load", getLocation);
-
+//GET LOCATION OF USER
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
@@ -8,37 +8,39 @@ function getLocation() {
         aler("Geolocation is not supported by this browser.");
     }
 }
-
 //DEFAULT LOCATION - NEW YORK
 var lat = 40.7128;
 var lng = -74.0060;
-
 //GLOBAL VARIABLES
 var myLatLng = {
     lat: lat,
     lng: lng
 };
-
+//MAP DISPLAY OPTIONS
 var mapOptions = {
     center: myLatLng,
     zoom: 7,
     mapTypeId: google.maps.MapTypeId.ROADMAP
 };
-
 //CREATE AUTOCOMPLETE OBJECTS FOR THE INPUT FIELDS
 var options = {
     fields: ["formatted_address", "geometry", "name"],
     strictBounds: false,
     types: ["address"],
 }
-
 //SET UP AUTOCOMPLETE FUNCTIONALITY
 var input1 = document.getElementById("from");
 var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
-
 var input2 = document.getElementById("to");
 var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
-
+//GEOCODER OBJECT
+var geocoder = new google.maps.Geocoder();
+//CREATE THE MAP
+var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
+//CREATE A DIRECTION SERVICE OBJECT SO I CAN USE ROUTE SERVICE LATER
+var directionsService = new google.maps.DirectionsService();
+//CREATE DIRECTION RENDER TO BE ABLE TO BIND TWO LOCATIONS
+var directionsDisplay = new google.maps.DirectionsRenderer();
 
 //SET UP CURRENT LOCATION
 function showPosition(position) {
@@ -63,9 +65,7 @@ function showPosition(position) {
     // ADD MARKER TO THE MAP
     marker.setMap(map);
 
-
     //CONVERT LON AND LAT INTO AN ADDRESS
-    var geocoder = new google.maps.Geocoder();
     var latLng = new google.maps.LatLng(myLatLng.lat, myLatLng.lng);
 
     if (geocoder) {
@@ -81,16 +81,6 @@ function showPosition(position) {
     }
 }
 
-//CREATE THE MAP
-var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
-
-//create a DirectionsService object to use the route method and get a result for our request
-var directionsService = new google.maps.DirectionsService();
-
-//CREATE DIRECTION RENDER TO BE ABLE TO BIND TWO LOCATIONS
-var directionsDisplay = new google.maps.DirectionsRenderer();
-
-
 //CALCROUTE FUNCTION
 function calcRoute() {
     //create request
@@ -104,6 +94,8 @@ function calcRoute() {
     //PASS REQUEST TO THE ROUTE METHOD
     directionsService.route(request, function (result, status) {
         if (status == google.maps.DirectionsStatus.OK) {
+            console.log(request.origin);
+            console.log(request.destination);            
 
             //GET DISTANCE AND TIME
             const output = document.querySelector('#output');
